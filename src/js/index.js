b57d7853733432fc83b549b6f31304c7e7449eb9
Steven Shropshire <sshrop@gmail.com>
2023-05-05 10:38:07 -0500

    Add styles and a few maps for keys and chromatic intervals.

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+const activeKeys = new Set();
+const keyboardKeyToIntervalMap = {
+  'a': 0,
+  'w': 1,
+  's': 2,
+  'e': 3,
+  'd': 4,
+  'f': 5,
+  't': 6,
+  'g': 7,
+  'y': 8,
+  'h': 9,
+  'u': 10,
+  'j': 11,
+  'k': 12,
+  'o': 13,
+  'l': 14,
+  'p': 15,
+  ';': 16,
+  "'": 17,
+};
+
+const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E'];
+
+function onActiveKeysChange() {
+  console.log(`Active Keys: ${Array.from(activeKeys).join(', ')}`);
+}
+
+document.addEventListener('keydown', (e) => {
+  const { key } = e;
+  if (!activeKeys.has(key)) {
+    activeKeys.add(key);
+    onActiveKeysChange();
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  const { key } = e;
+  if (activeKeys.has(key)) {
+    activeKeys.delete(key);
+    onActiveKeysChange();
+  }
+});
